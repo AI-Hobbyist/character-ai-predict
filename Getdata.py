21cc1a8972ced8c6ede337001129f18e5909c070
芙宁娜 <141395963+KOKOMI12345@users.noreply.github.com>
2024-03-29 21:58:24 +0800

    Add files via upload

--- Getdata.py
+++ Getdata.py
@@ -5,9 +5,11 @@ import torch
 from torch.utils.data import Dataset
 
 class AudioAugmentation:
-    def __init__(self, max_shift=2000, noise_factor=0.1):
+    def __init__(self, max_shift=2000, noise_factor=0.1, pitch_shift=2, speed_change=0.2):
         self.max_shift = max_shift
         self.noise_factor = noise_factor
+        self.pitch_shift = pitch_shift
+        self.speed_change = speed_change
 
     def __call__(self, waveform: np.ndarray) -> np.ndarray:
         # 随机裁剪
@@ -21,13 +23,21 @@ class AudioAugmentation:
         noise = torch.randn_like(torch.from_numpy(waveform)) * self.noise_factor
         augmented_waveform = waveform + noise.numpy()
 
+        # 变换音调
+        pitch_shift_amount = np.random.uniform(-self.pitch_shift, self.pitch_shift)
+        augmented_waveform = librosa.effects.pitch_shift(augmented_waveform[0], sr=44100, n_steps=pitch_shift_amount)
+
+        # 变换速度
+        speed_change_factor = 1 + np.random.uniform(-self.speed_change, self.speed_change)
+        augmented_waveform = librosa.effects.time_stretch(y=augmented_waveform, rate=speed_change_factor)
+
         # 数值范围控制
         augmented_waveform = np.clip(augmented_waveform, -1.0, 1.0)  # 限制数据范围在[-1, 1]之间
 
         return augmented_waveform
 
 class AudioDataset(Dataset):
-    def __init__(self, audio_files:list = None, labels:list = None, sr: int = 44100, duration: int = 3, transform=None):
+    def __init__(self, audio_files:list = [None], labels:list = [None], sr: int = 44100, duration: int = 3, transform=None):
         self.audio_files = audio_files
         self.labels = labels
         self.sr = sr
